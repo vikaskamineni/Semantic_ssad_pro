caac0e3c9e1ee425873d660300286c1575618ef4
vikas_007 <vikas.kamineni4007@gmail.com>
2016-09-23 13:04:09 +0530

    tagging func ref added

---
+++ tagging.js
@@ -0,0 +1,34 @@
+function func_tagging(stri)
+{
+    var span = document.createElement("span");
+    var prop = document.createAttribute("property");
+    
+    var prop2 = document.createAttribute("background-color");
+    if (window.getSelection().toString().length!==0) {    
+        console.log('highlighted');
+
+        prop.value=stri;
+	prop1.value = 'background-color:yellow';
+        span.setAttributeNode(prop);
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+            var range = sel.getRangeAt(0).cloneRange();
+            range.surroundContents(span);
+            sel.removeAllRanges();
+            sel.addRange(range);
+        }
+    }  
+}
+
+function toggle_tagging()
+{
+  var element = document.getElementById("page-wrap") ;
+  if(element.hasAttribute("hidden"))
+  {
+    element.removeAttribute("hidden");
+  }
+  else
+  {
+    element.setAttribute("hidden","");
+  }
+}

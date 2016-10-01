0b48b681ca142453dc412407aa8bdbaef7832508
vikas_007 <vikas.kamineni4007@gmail.com>
2016-10-01 21:44:58 +0530

    Update my_tagging.js

--- my_tagging.js
+++ my_tagging.js
@@ -45,6 +45,27 @@ function change_font(stri1,stri2)
         }
     }
 }
+
+
+function underline() {     
+    var span = document.createElement("span");   
+    var prop = document.createAttribute("style");  
+    if (window.getSelection().toString().length!=0)  
+    {       
+        prop.value="text-decoration:underline"   
+        span.setAttribute(prop); 
+        var sel = window.getSelection();    
+        if (sel.rangeCount) 
+        {   
+            var range = sel.getRangeAt(0).cloneRange();
+            range.surroundContents(span); 
+            sel.removeAllRanges(); 
+            sel.addRange(range) 
+        }
+    }
+}
+
+
 function conv_to_href()
 {
     //var span=document.createElement("span");

b3916b8457c0cdcf809ef3a4fcc90c512a2b5998
vikas_007 <vikas.kamineni4007@gmail.com>
2016-10-01 21:00:54 +0530

    Update search_function.js

--- search_function.js
+++ search_function.js
@@ -1,12 +1,12 @@
 function modify_startdate()
 {
- $(document).ready(function(){
-    $("[property='date-startdate']").each( function() {
+ $j(document).ready(function(){
+    $j("[property='date-startdate']").each( function() {
   
     var format = "Do MMM YYYY";
 
-    var $this = $( this );
-    var old_date = $.trim($this.text());
+    var $this = $j( this );
+    var old_date = $j.trim($this.text());
     var new_date = moment(old_date , 'YYYY-MM-DD').format( format );
 
     $this.text($this.text().replace(old_date, new_date));
@@ -15,13 +15,13 @@ function modify_startdate()
 }
 function modify_enddate()
 {
- $(document).ready(function(){
-    $("[property='date-enddate']").each( function() {
+ $j(document).ready(function(){
+    $j("[property='date-enddate']").each( function() {
   
     var format = "Do MMM YYYY";
 
-    var $this = $( this );
-    var old_date = $.trim($this.text());
+    var $this = $j( this );
+    var old_date = $j.trim($this.text());
     var new_date = moment(old_date , 'YYYY-MM-DD').format( format );
 
     $this.text($this.text().replace(old_date, new_date));

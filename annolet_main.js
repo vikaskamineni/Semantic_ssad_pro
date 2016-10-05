331d492ce6e7afd4282b31864706d1cd6ad862c0
vikas_007 <vikas.kamineni4007@gmail.com>
2016-10-05 09:20:18 +0530

    Update annolet_main.js

--- annolet_main.js
+++ annolet_main.js
@@ -233,16 +233,6 @@ function anno_remove_edit(xpath)
     $this.css("background-color","");
     //$this.text($this.text().replace(old_date, new_date));
   });
-    $j("[property = 'person-name-firstname']").each( function() {
-    //console.log("stri correct");
-    //var format = "MMMM Do, YYYY";
-
-    var $this = $j( this );
-    //var old_date = $j.trim($this.text());
-    //var new_date = moment(old_date ,["MM-DD-YYYY", "DD-MM-YYYY", "MMMM DD,YYYY", "MMMM Do YYYY", "Do MMM YYYY", "Do MMMM YYYY", "MMMM Do, YYYY"] ).format( format );
-    $this.css("background-color","");
-    //$this.text($this.text().replace(old_date, new_date));
-  });
   });
       
 }

4ee04aa2075af484b398567364e7deb4c3b125a1
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-03 18:23:55 +0530

    Create anno_main.js

---
+++ anno_main.js
@@ -0,0 +1,30 @@
+//main function which will execute other functions
+function annolet_main() {
+    disableAllLinks()  // it will disable all the links present in webpage iteratively
+}
+
+// funtion to disable all links
+function disableAllLinks(){
+    var anchors = document.getElementsByTagName("a");
+    for (var i = 0; i < anchors.length; i++) {
+        anchors[i].onclick = function() {return(false);};
+    }
+}
+
+// for exiting annolet
+$j("#annolet-exit-btn").click(function(e) {
+    if (e === undefined) {
+        e = window.event;
+    } // for IE
+    e.stopPropagation();
+});
+
+$(document).ready(function() {
+    // Get the div's width
+    var divWidth = $("#").width();
+    // Update div's width
+    $("#").css("width", divWidth + 'px');
+});
+    
+// calling main function
+annolet_main();

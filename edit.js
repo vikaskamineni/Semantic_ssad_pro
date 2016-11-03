b35e473d74d00c7bb25417e7dc8700635f55015f
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-03 23:59:48 +0530

    Update edit.js

--- edit.js
+++ edit.js
@@ -2,7 +2,7 @@ function anno_edit()
 {
         if (document.getElementsByTagName("body")[0].contentEditable= "true") 
         {
-                document.getElementsByTagName("body")[0].setAtrribute('contenteditable','false');
+                document.getElementsByTagName("body")[0].setAtrribute('contenteditable','');
         }
         else
         {

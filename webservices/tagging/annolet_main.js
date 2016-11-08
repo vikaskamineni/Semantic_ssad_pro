7296f8726f79228cbd10d7932bdc2e675ab6e570
vikaskamineni <vikaskamineni7@gmail.com>
2016-11-08 15:06:49 +0530

    added file

---
+++ webservices/tagging/annolet_main.js
@@ -0,0 +1,60 @@
+// function to get Xpath to passed element
+function anno_getXpathTo(element) {
+    if (element.id !== '') {
+        return "//*[@id='" + element.id + "']";
+    }
+    if (element === document.body) {
+        return "html/" + element.tagName.toLowerCase();
+    } //added 'html/' to generate a valid Xpath even if parent has no ID.
+    var ix = 0;
+    var siblings = element.parentNode.childNodes;
+    for (var i = 0; i < siblings.length; i++) {
+        var sibling = siblings[i];
+        if (sibling === element) {
+            return anno_getXpathTo(element.parentNode) + '/' + element.tagName.toLowerCase() + '[' + (ix + 1) + ']';
+        }
+        if (sibling.nodeType === 1 && sibling.tagName === element.tagName) {
+            ix++;
+        }
+    }
+}
+
+// function to evaluate element from Xpath
+function anno_getElementByXpath(xpath) {
+    return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+//------------------------------------------------------------------------
+function anno_rtag()
+{
+  var span = document.createElement("span");
+  var prop = document.createAttribute("property");
+  if (window.getSelection().toString().length!==0) {
+    prop.value = prompt("Enter the tag name you want to add");
+    span.setAttributeNode(prop);
+    var sel = window.getSelection();
+    if (sel.rangeCount) {
+      var range = sel.getRangeAt(0).cloneRange();
+      range.surroundContents(span);
+      sel.removeAllRanges();
+      sel.addRange(range);
+    }
+  }  
+}
+//------------------------------------------------------------------------
+
+
+
+//main function which will execute other functions
+function do_tagging() {
+    document.onclick = function(event) {
+        if (event === undefined) {
+            event = window.event;
+        } // for IE
+        var target = 'target' in event ? event.target : event.srcElement; // for IE
+        var root = document.compatMode === 'CSS1Compat' ? document.documentElement : document.body;
+        var xpath = anno_getXpathTo(target);
+        console.log(xpath);
+          anno_rtag();
+    };
+}

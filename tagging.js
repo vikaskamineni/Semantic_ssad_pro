5b8a09cc63278c95e253671defb599fbaf7ff050
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-09 20:42:04 +0530

    Update tagging.js

--- tagging.js
+++ tagging.js
@@ -32,5 +32,34 @@ function func_tagging(stri)
   {
     element.setAttribute("hidden","");
   }
+}*/
+function tag_function()
+{
+    //if(cnt%2==1)
+    //{
+    console.log("remove new hidden");
+    var temp=document.getElementById("page_wrap_new");
+    console.log(temp);
+    document.getElementById("page_wrap").removeAttribute("hidden");
+    //}
+    //else
+    //{
+    if(document.getElementById("page_wrap_new").hasAttribute("hidden"))
+    {
+    }
+    else
+    {
+        console.log("add hidden");
+        document.getElementById("page_wrap_new").setAttribute("hidden",true);
+    }
+    if(document.getElementById("search-wrap").hasAttribute("hidden"))
+    {
+    }
+    else
+    {
+        console.log("add hidden");
+        document.getElementById("search-wrap").setAttribute("hidden",true);
+    }
+    
+ 
 }
-*/

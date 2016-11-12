9202c8966cb49485d71e430091901516b100f704
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-12 19:31:13 +0530

    Update my_tagging.js

--- my_tagging.js
+++ my_tagging.js
@@ -2,28 +2,55 @@
 
 function tagging_function(stri1,stri2)
 {
-    var span = document.createElement("span");
-    var prop1 = document.createAttribute("property");
-    var prop2 = document.createAttribute("style");
+    anno_btn = 16;
+    document.onclick = function(event) {
+        if (event === undefined) {
+            event = window.event;
+        } // for IE
+        var target = 'target' in event ? event.target : event.srcElement; // for IE
+        //console.log(target);
+        var root = document.compatMode === 'CSS1Compat' ? document.documentElement : document.body;
+        var xpath = anno_getXpathTo(target);
+        //console.log(xpath);
+        var ele = anno_getElementByXpath(xpath);
+        //console.log(ele);
+        var span = document.createElement("span");
+        var prop1 = document.createAttribute("property");
+        var prop2 = document.createAttribute("style");
 
-    if (window.getSelection().toString().length!=0)
-    {
-        console.log('highlighted');
-        //tagName = prompt('tagName');
-        //tagInfo = prompt('tagInfo');
-        prop2.value = stri1;
-        prop1.value = stri2;
-        console.log(stri2);
-        span.setAttributeNode(prop2);
-        span.setAttributeNode(prop1);
-        var sel = window.getSelection();
-        if (sel.rangeCount) {
-            var range = sel.getRangeAt(0).cloneRange();
-            range.surroundContents(span);
-            sel.removeAllRanges();
-            sel.addRange(range);
+        if (window.getSelection().toString().length!=0)
+        {
+            console.log('highlighted');
+            //tagName = prompt('tagName');
+            //tagInfo = prompt('tagInfo');
+            prop2.value = stri1;
+            prop1.value = stri2;
+            console.log(stri2);
+            span.setAttributeNode(prop2);
+            span.setAttributeNode(prop1);
+            var sel = window.getSelection();
+            if (sel.rangeCount) {
+                var range = sel.getRangeAt(0).cloneRange();
+                range.surroundContents(span);
+                sel.removeAllRanges();
+                sel.addRange(range);
+            }
         }
-    }
+        var sel = window.getSelection();
+        var temp = window.getSelection().toString().length;
+        console.log(temp);
+        var pre = anno_getElementByXpath(xpath);
+        var offset = getCharOffsetRelativeTo(pre, sel.anchorNode, sel.anchorOffset);
+        console.log(offset);
+        var start = offset;
+        var end = offset + temp;
+        var currentLocation = window.location.href;
+        var obj = JSON.parse(jsonStr);
+        obj['change'].push({"xpath":xpath,"url":currentLocation,"func_triggered":anno_btn,"start_offset":start,"end_offset":end});
+        jsonStr = JSON.stringify(obj);
+        console.log("inside func");
+        console.log(jsonStr);
+    };
 }
 function tag_function()
 {
@@ -320,7 +347,7 @@ function bold_tag(stri)
             console.log(stri);
             span.setAttributeNode(prop2);
             span.setAttributeNode(prop1);
-            span.setAttributeNode(span_id);
+            //span.setAttributeNode(span_id);
             var sel = window.getSelection();
             if (sel.rangeCount) {
                 var range = sel.getRangeAt(0).cloneRange();

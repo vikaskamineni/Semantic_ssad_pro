d4f31740a8971be600636014a25006b7e16aa4f4
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-12 10:59:45 +0530

    Update annolet_main.js

--- webservices/translation/annolet_main.js
+++ webservices/translation/annolet_main.js
@@ -28,6 +28,26 @@ function get_languagetrans(str,fr,to){
 
 //function for getting phonetic
 function anno_language(xpath) {
+  var span = document.createElement("span");
+  var prop = document.createAttribute("property");
+  var span_id = document.createAttribute("id");
+  ID = ID + 1;
+  if (window.getSelection().toString().length!=0)
+  {
+      prop.value = "language";
+      span_id.value = ID;
+      span.setAttributeNode(prop);
+      span.setAttributeNode(span_id);
+      var sel = window.getSelection();
+      if(sel.rangeCount){
+          var range = sel.getRangeAt(0).cloneRange();
+          range.surroundContents(span);
+          sel.removeAllRanges();
+          sel.addRange(range);
+      }
+  }
+  var span_ele = document.getElementById(ID);
+  var fin_xpath = anno_getXpathTo(span_ele);
   var clicked_element = anno_getElementByXpath(xpath);
   //if element is already highlighted
   if (clicked_element.id == "mark" || clicked_element.id == "annolet") {
@@ -35,8 +55,8 @@ function anno_language(xpath) {
   }
   else {
   //if element is already translated
-  if (anno_getElementByXpath(xpath).id != "language" || !(anno_getElementByXpath(xpath).id)) {
-    var text_to_translate = $j(anno_getElementByXpath(xpath)).html();
+  if (anno_getElementByXpath(fin_xpath).id != "language" || !(anno_getElementByXpath(fin_xpath).id)) {
+    var text_to_translate = $j(anno_getElementByXpath(fin_xpath)).html();
     get_languagetrans(text_to_translate,'en','hi');
     var timer = window.setInterval
     (
@@ -45,7 +65,8 @@ function anno_language(xpath) {
         if(typeof language_trans !== "default_value")
         {
           console.log("text changing");
-          $j(anno_getElementByXpath(xpath)).text(language_trans);
+          $j(anno_getElementByXpath(fin_xpath)).text(language_trans);
+          $j(anno_getElementByXpath(fin_xpath)).id("language");
           language_trans = "default_value";
           window.clearInterval(timer);
         }
@@ -61,6 +82,7 @@ function anno_language(xpath) {
         console.log('already translated');
     }
   }
+  return fin_xpath;
 }
 
 //------------------------------------------------------------------------
@@ -77,10 +99,10 @@ function run_langtrans() {
         var target = 'target' in event ? event.target : event.srcElement; // for IE
         var root = document.compatMode === 'CSS1Compat' ? document.documentElement : document.body;
         var xpath = anno_getXpathTo(target);
-        anno_language(xpath);
+        var final_xpath = anno_language(xpath);
         var currentLocation = window.location;
         var obj = JSON.parse(jsonStr);
-        obj['change'].push({"xpath":xpath,"url":currentLocation,"func_triggered":anno_btn});
+        obj['change'].push({"xpath":final_xpath,"url":currentLocation,"func_triggered":anno_btn});
         jsonStr = JSON.stringify(obj);
     };
 }

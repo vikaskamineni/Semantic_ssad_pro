317d9c19d2553c4f34254013aa50d0b02ba727db
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-12 22:22:52 +0530

    Create database.js

---
+++ database.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyDaG5qULLqFA7S6Tk-dMuv9ilVXbrTjmwg",
+    authDomain: "ssad43-12c23.firebaseapp.com",
+    databaseURL: "https://ssad43-12c23.firebaseio.com",
+    storageBucket: "ssad43-12c23.appspot.com",
+    messagingSenderId: "354327773473"
+  };
+  firebase.initializeApp(config);

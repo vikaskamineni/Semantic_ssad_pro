0ea84c61e67e3a81ac48592baa6043a1dcb6457f
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-12 11:21:19 +0530

    Update anno_main.js

--- anno_main.js
+++ anno_main.js
@@ -1,7 +1,7 @@
 //main function which will execute other functions
 var jsonStr = '{ "change":[] }';
 var anno_btn = 0;
-var ID = "123";
+var ID = 0;
 function annolet_main() {
     disableAllLinks(); // it will disable all the links present in webpage iteratively
     //var jsonStr = { "change":[] };

66339e82dd7fb89466397e11cbc296eda908d512
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-13 19:25:59 +0530

    Update persist.js

--- persist.js
+++ persist.js
@@ -2,5 +2,14 @@ function store_changes()
 {
 	console.log("outside func");
 	console.log(jsonStr);
+	data = {};
+	data['url'] = window.location.href;
+	data['json'] = jsonStr;
+	$.post("http://54.71.184.131/save.php",data,function(data,status)
+	{
+		alert('Success');
+		alert(data);
+	}
+		);
 	window.alert(jsonStr);
 }

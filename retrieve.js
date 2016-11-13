c8bbd53b8b5a5f3be3880138653f51db0df1db41
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-13 13:27:47 +0530

    Update retrieve.js

--- retrieve.js
+++ retrieve.js
@@ -20,7 +20,46 @@ function wrapText(elem, start, length, tagName ,st) {
     elem.appendChild(text3);
 }
 
-
+function wrapa(elem , start, length, tagName, st) {
+    var before = elem.textContent.substr(0, start);
+    var after = elem.textContent.substr(start + length, elem.textContent.length - length);
+    var letters = elem.textContent.substr(start, length);
+    elem.innerHTML = '';
+    var text1=document.createTextNode(before);
+    var text2=document.createElement('a');
+    //var prop1 = document.createAttribute('property');
+    //var prop2 = document.createAttribute('style');
+    var text3=document.createTextNode(after);
+    //text2.property = tagName;
+    text2.style = st;
+    text2.href = tagName;
+    //text2.setAttributeNode(prop1);
+    //text2.setAttributeNode(prop2);
+    text2.innerHTML = letters;
+    elem.appendChild(text1);
+    elem.appendChild(text2);
+    elem.appendChild(text3);
+}
+function wrapheading(elem , start, length, tagName, st) {
+    var before = elem.textContent.substr(0, start);
+    var after = elem.textContent.substr(start + length, elem.textContent.length - length);
+    var letters = elem.textContent.substr(start, length);
+    elem.innerHTML = '';
+    var text1=document.createTextNode(before);
+    var text2=document.createElement('h1');
+    //var prop1 = document.createAttribute('property');
+    //var prop2 = document.createAttribute('style');
+    var text3=document.createTextNode(after);
+    //text2.property = tagName;
+    //text2.style = st;
+    //text2.href = tagName;
+    //text2.setAttributeNode(prop1);
+    //text2.setAttributeNode(prop2);
+    text2.innerHTML = letters;
+    elem.appendChild(text1);
+    elem.appendChild(text2);
+    elem.appendChild(text3);
+}
 function get_languagetrans(str,fr,to){
 
   var xhr = new XMLHttpRequest();
@@ -224,17 +263,18 @@ function retrieve_changes()
       }
       if (anno_btn == 18)
       {
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,"text-color:blue");  
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<a style = "text-color:blue" href = tagName >" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</a>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if (anno_btn == 19)
       {
-        wrapText(ele, start_offset, length, tagName ,"font-weight:bold");
+        wrapa(ele, start_offset, length, tagName ,"font-weight:bold");
         /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span style = "font-wieght:bold" >" +
@@ -245,13 +285,14 @@ function retrieve_changes()
       }
       if (anno_btn == 20)
       {
-        $j(ele).html(function(i,val) {
+        wrapheading(ele, start_offset, length, tagName ,null);  
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<h1>" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</h1>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if(tagName == "event-date-startdate"|| tagName == "event-date-enddate" || tagName == "event-location-street" ||tagName == "event-location-area" ||tagName == "event-location-city" ||tagName == "organization-owner" ||tagName == "organisation-employee" ||tagName == "organization-contact" ||tagName == "organization-location-street" ||tagName == "organization-location-area" ||tagName == "organization-location-city" || tagName == "person-name-firstname" ||tagName == "person-name-secondname" ||tagName == "person-address-street" ||tagName == "person-address-city" || tagName == "person-address-area" || tagName == "person-contact" || tagName == "date-startdate" || tagName == "date-enddate" || tagName == "currency-rupee" || tagName == "currency-dollar" || tagName == "currency-euro" || tagName == "unit-si" || tagName == "unit-cgi" || tagName == "unit-fps")
       {

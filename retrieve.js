c68d8fdc5cc7eaad50fd627587edfc8e62ebe278
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-13 11:11:33 +0530

    Update retrieve.js

--- retrieve.js
+++ retrieve.js
@@ -1,5 +1,25 @@
 var $j = jQuery.noConflict();
 
+function wrapText(elem, start, length, tagName ,st) {
+    var before = elem.textContent.substr(0, start);
+    var after = elem.textContent.substr(start + length, elem.textContent.length - length);
+    var letters = elem.textContent.substr(start, length);
+    elem.innerHTML = '';
+    var text1=document.createTextNode(before);
+    var text2=document.createElement('span');
+    var prop1 = document.createAttribute('property');
+    var prop2 = document.createAttribute('style');
+    var text3=document.createTextNode(after);
+    prop1.value = tagName;
+    prop2.value = st;
+    text2.setAttributeNode(prop1);
+    text2.setAttributeNode(prop2);
+    text2.innerHTML = letters;
+    elem.appendChild(text1);
+    elem.appendChild(text2);
+    elem.appendChild(text3);
+}
+
 
 function get_languagetrans(str,fr,to){
 
@@ -56,26 +76,28 @@ function retrieve_changes()
       var xpath = obj['change'][j].xpath;
       var start_offset = obj['change'][j].start_offset;
       var end_offset = obj['change'][j].end_offset;
+      var length = start_offset - end_offset;
       var anno_btn = obj['change'][j].func_triggered;
       var tagName = obj['change'][j].tagName;
       var ele = anno_getElementByXpath(xpath);
       if (anno_btn == 1)
       {
-        //wrapText(ele,start_offset,end_offset-start_offset);
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,null);
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span property=tagName>" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</span>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if (anno_btn == 2)
       {
+        wrapText(ele, start_offset, length, tagName ,null);
         var stri = $j(ele).html(function(i,val) {
           return val.substr(start_offset,end_offset-start_offset);
         });
-        var text_to_translate = $j(anno_getElementByXpath(xpath)).html();
+        var text_to_translate = stri;
         var language_trans = get_languagetrans(text_to_translate,'en','hi');
         var timer = window.setInterval
         (
@@ -98,10 +120,11 @@ function retrieve_changes()
       }
       if (anno_btn == 3)
       {
+        wrapText(ele, start_offset, length, tagName ,null);
         var stri = $j(ele).html(function(i,val) {
           return val.substr(start_offset,end_offset-start_offset);
         });
-        var text_to_translate = $j(anno_getElementByXpath(xpath)).html();
+        var text_to_translate = stri;
         var phonetic_trans = get_languagetrans(text_to_translate,'en','hi');
         var timer = window.setInterval
         (
@@ -124,73 +147,80 @@ function retrieve_changes()
       }
       if (anno_btn == 11)
       {
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,"background-color:yellow");
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span style = "background-color:yellow" >" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</span>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if (anno_btn == 12)
       {
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,"background-color:red");
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span style = "background-color:red" >" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</span>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if (anno_btn == 13)
       {
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,"font-family:arial");
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span style = "font-family:arial" >" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</span>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if (anno_btn == 14)
       {
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,"font-family:courier");
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span style = "font-family:courier" >" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</span>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if (anno_btn == 15)
       {
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,"font-family:Helvetica");
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span style = "font-family:Helvetica" >" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</span>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if (anno_btn == 16)
       {
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,"font-family:Times");
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span style = "font-family:Times" >" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</span>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if (anno_btn == 17)
       {
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,"text-decoration:underline");
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span style = "text-decoration:underline" >" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</span>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if (anno_btn == 18)
       {
@@ -204,13 +234,14 @@ function retrieve_changes()
       }
       if (anno_btn == 19)
       {
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,"font-weight:bold");
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span style = "font-wieght:bold" >" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</span>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       if (anno_btn == 20)
       {
@@ -224,13 +255,14 @@ function retrieve_changes()
       }
       if(tagName == "event-date-startdate"|| tagName == "event-date-enddate" || tagName == "event-location-street" ||tagName == "event-location-area" ||tagName == "event-location-city" ||tagName == "organization-owner" ||tagName == "organisation-employee" ||tagName == "organization-contact" ||tagName == "organization-location-street" ||tagName == "organization-location-area" ||tagName == "organization-location-city" || tagName == "person-name-firstname" ||tagName == "person-name-secondname" ||tagName == "person-address-street" ||tagName == "person-address-city" || tagName == "person-address-area" || tagName == "person-contact" || tagName == "date-startdate" || tagName == "date-enddate" || tagName == "currency-rupee" || tagName == "currency-dollar" || tagName == "currency-euro" || tagName == "unit-si" || tagName == "unit-cgi" || tagName == "unit-fps")
       {
-        $j(ele).html(function(i,val) {
+        wrapText(ele, start_offset, length, tagName ,null);
+        /*$j(ele).html(function(i,val) {
           return val.substr(0,start_offset) +
                 "<span property = tagName >" +
                 val.substr(start_offset,end_offset-start_offset ) +
                 "</span>" +
                 val.substr(end_offset);
-        });
+        });*/
       }
       
       

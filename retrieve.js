3dbcefaf9a59470f7986ba385df394b04a1e5ed0
vikas_007 <vikas.kamineni4007@gmail.com>
2016-11-14 21:23:41 +0530

    Update retrieve.js

--- retrieve.js
+++ retrieve.js
@@ -65,7 +65,13 @@ function retrieve_changes()
       jsonstr = xhr.responseText;
     }
   }
-  jsonstr = '{"change":[{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":17,"start_offset":4,"end_offset":13,"tagName":"underline"},{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":20,"start_offset":0,"end_offset":3,"tagName":"heading"},{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":14,"start_offset":22,"end_offset":29,"tagName":"courier"},{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":18,"start_offset":34,"end_offset":58,"tagName":"www.abc.com"},{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":11,"start_offset":59,"end_offset":67,"tagName":"yellow"},{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":19,"start_offset":78,"end_offset":87,"tagName":"bold"}]}';
+  jsonstr = '{"change":['+
+    '{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":17,"start_offset":4,"end_offset":13,"tagName":"underline"},'+
+    '{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":20,"start_offset":0,"end_offset":3,"tagName":"heading"},'+
+    '{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":14,"start_offset":22,"end_offset":29,"tagName":"courier"},'+
+    '{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":18,"start_offset":34,"end_offset":58,"tagName":"www.abc.com"},'+
+    '{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":11,"start_offset":59,"end_offset":67,"tagName":"yellow"},'+
+    '{"xpath":"//*[@id='main']/p[1]","url":"http://www.w3schools.com/sql/sql_join_left.asp#","func_triggered":19,"start_offset":78,"end_offset":87,"tagName":"bold"}]}';
   var obj = JSON.parse(jsonstr);
   console.log(obj);
   var j=0;
